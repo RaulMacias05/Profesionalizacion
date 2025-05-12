22f22522cdf13c5b10377c2aed3bf2100e80de9a
Alejandro Nuñez <ale.nu.rdz@gmail.com>
2025-05-11 19:21:36 -0600

    copia template

--- ventas/urls.py
+++ ventas/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ventas, registrar_venta, listar_ventas
+from .views import ventas, registrar_venta, listar_ventas, registrar_venta_copy
+from . import views
 
 app_name = 'ventas'
 
 urlpatterns = [
     path('', ventas, name='ventas'),
     path('registrar/', registrar_venta, name='registrar_venta'),
-    path('listar/', listar_ventas, name='listar_ventas')
+    path('listar/', listar_ventas, name='listar_ventas'),
+    path('registrar-copy/', views.registrar_venta_copy, name='registrar_venta_copy'),
 ]
